3e84d7905cfc18d440af65dba3795ea2d5ee4762
AndreasResa <jrresa11@gmail.com>
2025-02-12 21:02:28 +0800

    exercise3

---
+++ exercise3.js
@@ -0,0 +1,25 @@
+const moment = require('moment');
+const users = require('./users');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.status(200).json({
+    message: 'This is the home page'
+}))
+
+app.get('/about', (req, res) => res.status(200).json({
+    status: 'success',
+    message: 'response success',
+    description: 'exercise #3',
+    date: moment().format('MMMM Do YYYY, h:mm:ss a')
+}))
+
+app.get('/users', (req, res) => res.status(200).json(users))
+
+
+
+const hostname = '127.0.0.1';
+const port = 3000;
+app.listen(port, hostname, ()=>console.log(`Server running at http://localhost:${port}`));
+
+
